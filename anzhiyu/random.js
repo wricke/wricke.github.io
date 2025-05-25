7be75dbc0caefac9288b2cb83fcc7ed6a0a395d9
wricke <1037420376@qq.com>
2025-05-25 15:33:33 +0800

    Site updated: 2025-05-25 15:33:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/24/Mysql/","2025/05/24/hello-world/"];function toRandomPost(){
+var posts=["2025/05/24/hello-world/","2025/05/24/Mysql/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
